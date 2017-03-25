40a26ed9ca69a9a08b3e03583cb90e2cabbc1add
poming\pomingpo <roy989898@gmail.com>
2017-03-25 22:36:15 +0800

    create project

---
+++ polyArduinoVS/Debug/polyArduinoVS.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\pomingpo\AppData\Local\Temp\VMBuilds\polyArduinoVS\uno\Debug\polyArduinoVS.cpp.o: \
+ C:\Users\pomingpo\AppData\Local\Temp\VMBuilds\polyArduinoVS\uno\Debug\polyArduinoVS.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Community\Common7\IDE\Extensions\2r25oje5.2cf\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
---
+++ polyArduinoVS/polyArduinoVS.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		polyArduinoVS.ino
+ Created:	2017/3/25 下午 10:34:12
+ Author:	pomingpo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
